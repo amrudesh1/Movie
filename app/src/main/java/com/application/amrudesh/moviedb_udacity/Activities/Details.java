21e853332a975c2251bfdc0a376c3c2d109e0078
Amrudesh Balakrishnan <amrbal@gmail.com>
2019-02-18 22:04:47 +0530

    Merge remote-tracking branch 'origin/test' into test
    # Conflicts:
    #	app/src/main/java/com/application/amrudesh/moviedb_udacity/Activities/Details.java
    #	app/src/main/java/com/application/amrudesh/moviedb_udacity/Activities/MainActivity.java
    #	app/src/main/java/com/application/amrudesh/moviedb_udacity/Util/Constants.java

--- app/src/main/java/com/application/amrudesh/moviedb_udacity/Activities/Details.java
+++ app/src/main/java/com/application/amrudesh/moviedb_udacity/Activities/Details.java
@@ -1,12 +1,14 @@
 package com.application.amrudesh.moviedb_udacity.Activities;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
@@ -57,6 +59,7 @@ public class Details extends AppCompatActivity implements Serializable {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        Button btn = (Button) findViewById(R.id.review_btn);
         mvData = new Movie();
         movie =(Movie) getIntent().getSerializableExtra("movie");
         movieId = movie.getMovieID();
@@ -68,7 +71,14 @@ public class Details extends AppCompatActivity implements Serializable {
         setBtnStatus();
         Log.i("Movie",movie.getFavBtn().toString());
 
-
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getApplicationContext(),Review.class);
+                intent.putExtra("movie",movie);
+                startActivity(intent);
+            }
+        });
         likeButton.setOnLikeListener(new OnLikeListener() {
             @Override
             public void liked(LikeButton likeButton) {
@@ -165,6 +175,7 @@ public class Details extends AppCompatActivity implements Serializable {
         queue.add(jsonObjectRequest);
 
     }
+
 
 
 
